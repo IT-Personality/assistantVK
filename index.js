69ae17c4ec403bc88b1bd89e826b10d018a780f7
IŦ-Pers๏nality <92996537+IT-Personality@users.noreply.github.com>
2024-08-26 08:50:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+// const puppeteer = require('puppeteer');
+
+// (async () => {
+//   try {
+//     const browser = await puppeteer.launch({
+//       executablePath: '/opt/render/.cache/puppeteer/chrome/linux-127.0.6533.119/chrome-linux64/chrome'
+//     });
+//     const page = await browser.newPage();
+//     await page.goto('https://www.google.com');
+//     console.log('Page title:', await page.title());
+//     await browser.close();
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+// })();
+
 const puppeteer = require('puppeteer');
 
 (async () => {
-  try {
-    const browser = await puppeteer.launch({
-      executablePath: '/opt/render/.cache/puppeteer/chrome/linux-127.0.6533.119/chrome-linux64/chrome'
-    });
-    const page = await browser.newPage();
-    await page.goto('https://www.google.com');
-    console.log('Page title:', await page.title());
-    await browser.close();
-  } catch (error) {
-    console.error('Error:', error);
-  }
+  const browserFetcher = puppeteer.createBrowserFetcher();
+  const revisionInfo = browserFetcher.revisionInfo('127.0.0'); // замените на нужный номер версии
+
+  console.log('Chromium executable path:', revisionInfo.executablePath);
 })();
